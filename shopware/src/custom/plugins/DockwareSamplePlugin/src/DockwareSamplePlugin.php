371324b81b9f6c9662717338a300b5cf4abf5e15
emschu <emschu@mailbox.org>
2020-11-12 09:57:13 +0100

    add blanko shopware plugin + dockware setup

---
+++ shopware/src/custom/plugins/DockwareSamplePlugin/src/DockwareSamplePlugin.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace DockwareSamplePlugin;
+
+use Shopware\Core\Framework\Plugin;
+
+class DockwareSamplePlugin extends Plugin
+{
+
+    public function install(InstallContext $context): void
+    {
+        // your code you need to execute while installation
+    }
+}
