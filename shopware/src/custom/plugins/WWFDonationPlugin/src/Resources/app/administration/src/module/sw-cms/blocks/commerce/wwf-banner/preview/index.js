9ede9270eeea3cd433a022e8df813926b2955ed5
emschu <emschu@mailbox.org>
2021-04-23 11:21:07 +0200

    [sw6] refine admin/storefront preview of cms block element

--- shopware/src/custom/plugins/WWFDonationPlugin/src/Resources/app/administration/src/module/sw-cms/blocks/commerce/wwf-banner/preview/index.js
+++ shopware/src/custom/plugins/WWFDonationPlugin/src/Resources/app/administration/src/module/sw-cms/blocks/commerce/wwf-banner/preview/index.js
@@ -1,4 +1,5 @@
 import template from './sw-cms-preview-block-commerce-wwf-banner.html.twig';
+import './sw-cms-preview-block-commerce-wwf-banner.scss'
 
 Shopware.Component.register('sw-cms-preview-block-commerce-wwf-banner', {
     template,
--- shopware/src/custom/plugins/WWFDonationPlugin/src/Resources/app/administration/src/module/sw-cms/elements/wwf-banner/component/index.js
+++ shopware/src/custom/plugins/WWFDonationPlugin/src/Resources/app/administration/src/module/sw-cms/elements/wwf-banner/component/index.js
@@ -1,7 +1,8 @@
 import template from './sw-cms-el-wwf-banner.html.twig';
+import './sw-cms-el-wwf-banner.scss';
 
 const {Component} = Shopware;
 
 Component.register('sw-cms-el-wwf-banner', {
-    template
+    template,
 });
