31da9d5e43e0bdea0b91c74e15ca70a0da567cbf
emschu <emschu@mailbox.org>
2021-05-06 17:16:09 +0200

    [sw6] add donation report entity definitions + design and define this db table via a migration, add ext-json to composer project file, add mail template migration for donation report mail
    Signed-off-by: emschu <emschu@mailbox.org>

---
+++ shopware/src/custom/plugins/WWFDonationPlugin/src/Migration/Migration1620311459DonationReport.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace WWFDonationPlugin\Migration;
+
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\Migration\MigrationStep;
+
+class Migration1620311459DonationReport extends MigrationStep
+{
+
+    public function getCreationTimestamp(): int
+    {
+        return 1620311459;
+    }
+
+    public function update(Connection $connection): void
+    {
+        $sql = <<<SQL
+CREATE TABLE IF NOT EXISTS `wwf_donation_report` (
+    `id` BINARY(16) NOT NULL,
+    `name` VARCHAR(255) COLLATE utf8mb4_unicode_ci,
+    `order_count` INT(11) NOT NULL,
+    `interval_mode` VARCHAR(16) NOT NULL,
+    `is_regular` TINYINT(1) NOT NULL DEFAULT true,
+    `total_amount` DOUBLE NOT NULL,
+    `campaign_details` JSON NOT NULL,
+    `mail_content` MEDIUMTEXT COLLATE utf8mb4_unicode_ci,
+    `start_date` DATETIME(3) NOT NULL,
+    `end_date` DATETIME(3) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL,
+    `updated_at` DATETIME(3),
+    PRIMARY KEY (`id`)
+)
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4
+    COLLATE = utf8mb4_unicode_ci;
+SQL;
+        $connection->executeStatement($sql);
+    }
+
+    public function updateDestructive(Connection $connection): void
+    {
+        // Note: We do not delete the db table here to prevent data loss
+    }
+}
