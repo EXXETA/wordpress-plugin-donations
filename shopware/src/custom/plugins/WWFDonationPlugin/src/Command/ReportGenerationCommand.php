82c529e45bdb556d34cd94a0454b0bc7841f578f
emschu <emschu@mailbox.org>
2021-05-20 11:45:04 +0200

    [sw6] add debug cli cmd to test report generation
    Signed-off-by: emschu <emschu@mailbox.org>

--- shopware/src/custom/plugins/WWFDonationPlugin/src/Command/ReportGenerationCommand.php
+++ shopware/src/custom/plugins/WWFDonationPlugin/src/Command/ReportGenerationCommand.php
@@ -5,6 +5,7 @@ namespace WWFDonationPlugin\Command;
 
 
 use exxeta\wwf\banner\DonationPluginInterface;
+use exxeta\wwf\banner\model\ReportGenerationModel;
 use exxeta\wwf\banner\ReportGenerator;
 use exxeta\wwf\banner\SettingsManagerInterface;
 use Symfony\Component\Console\Command\Command;
@@ -14,6 +15,9 @@ use WWFDonationPlugin\Service\ShopwareReportHandler;
 
 /**
  * Class ReportGenerationCommand
+ * This command is hidden in non-dev environments.
+ *
+ * Use this for debugging purposes only!
  *
  * @package WWFDonationPlugin\Command
  */
@@ -46,17 +50,27 @@ class ReportGenerationCommand extends Command
     protected function configure(): void
     {
         $this->setDescription('Manual trigger to start report generation process for ordered WWF products. For debugging only!');
-        // hide in prod environments
-        $isDev = boolval(getenv('APP_ENV') == 'dev');
-        $this->setHidden($isDev);
+        // hide/disable in prod environments
+        $this->setHidden(!$this->isEnabled());
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         // weekly and now
         $reportModel = ReportGenerator::getReportModel(SettingsManagerInterface::REPORT_INTERVAL_MODE_WEEKLY, new \DateTime());
-        ReportGenerator::generateReport($reportModel, $this->pluginInstance, $this->reportHandler);
+        // pass these values to a new instance
+        $newReportModel = new ReportGenerationModel($reportModel->getStartDate(), $reportModel->getEndDate(), $reportModel->getIntervalMode(), false, true);
+
+        ReportGenerator::generateReport($newReportModel, $this->pluginInstance, $this->reportHandler);
+
+        $reportCounter = $this->pluginInstance->getSettingsManagerInstance()->getReportCounter();
+        $output->writeln(sprintf('Generated donation report #%d', $reportCounter));
 
         return 0;
+    }
+
+    public function isEnabled()
+    {
+        return boolval(getenv('APP_ENV') == 'dev');
     }
 }
